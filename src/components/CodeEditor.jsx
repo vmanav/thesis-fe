b085bd576c3ad70a0e26cacc08fcfbb1a5ffeab9
Manav Verma <zmanav.1999@gmail.com>
2025-07-20 17:44:57 +0100

    add more components

--- src/components/CodeEditor.jsx
+++ src/components/CodeEditor.jsx
@@ -2,7 +2,7 @@ import Editor from '@monaco-editor/react';
 
 const CodeEditor = ({ code, setCode }) => (
     <Editor
-        height="200px"
+        height="100%"
         defaultLanguage="rust"
         value={code}
         onChange={(val) => setCode(val)}
--- src/components/CodeOutput.jsx
+++ src/components/CodeOutput.jsx
@@ -1,5 +1,7 @@
-const CodeOutput = ({ output }) => (
-    <pre className="output-box">{output}</pre>
+const CodeOutput = ({ output, className }) => (
+    <div className={className}>
+        <pre className="output-box">{output}</pre>
+    </div >
 );
 
 export default CodeOutput;
