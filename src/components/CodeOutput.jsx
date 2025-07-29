ef7f1f1cef66fef9ec7b977ab62837ce13baa96a
Manav Verma <zmanav.1999@gmail.com>
2025-07-29 15:09:53 +0100

    add refactoring

--- src/components/CodeOutput.jsx
+++ src/components/CodeOutput.jsx
@@ -1,6 +1,34 @@
-const CodeOutput = ({ output, className }) => (
+import classNames from "classnames";
+
+const CodeOutput = ({
+    output,
+    inputRequired,
+    input,
+    sendInput,
+    setInput,
+    className,
+    hasError
+}) => (
     <div className={className}>
-        <pre className="output-box">{output}</pre>
+        <pre
+            className={classNames(
+                'output-container',
+                { 'output-container--error': hasError, }
+            )}
+        >
+            {output}
+            {inputRequired && (
+                <div className="input-box-container">
+                    <input
+                        className="input-box"
+                        value={input}
+                        onChange={event => setInput(event.target.value)}
+                        onKeyDown={event => { if (event.key === 'Enter') sendInput(); }}
+                        autoFocus
+                    />
+                </div>
+            )}
+        </pre>
     </div >
 );
 
--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,20 +1,31 @@
 import { socketDataTypes } from "../constants/socketDataTypes";
 
-export function createSocket(code, onMessage, onInputPrompt, onComplete, onError) {
-    const sock = new WebSocket("ws://localhost:8000/ws/execute");
+export function createSocket(
+    code,
+    onMessage,
+    onInputPrompt,
+    onComplete,
+    onError,
+    setHasError
+) {
+    const sock = new WebSocket("ws://localhost:8000/execute");
 
     sock.onopen = () => {
         sock.send(JSON.stringify({ code }));
     };
 
     sock.onmessage = ({ data }) => {
+        setHasError(false);
         const socketData = JSON.parse(data);
+        console.log("socketData:", socketData);
+        console.log("socketData type:", socketData.type);
         if (socketData.type === socketDataTypes.INPUT_PROMPT) {
-            onInputPrompt(socketData.prompt)
+            onInputPrompt(true)
         } else if (socketData.type === socketDataTypes.OUTPUT) {
             onMessage(socketData.content);
         } else if (socketData.type === socketDataTypes.STDERR) {
-            onError(socketData.content);
+            console.log("Error received from server:", socketData.message);
+            onError(socketData.message);
         } else if (socketData.type === socketDataTypes.EXECUTED) {
             onComplete();
             sock.close();
